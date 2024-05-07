91d30f4141c00771f73e590ed209bd9769fcc8a0
ThirteenYue <1304131318@qq.com>
2024-05-07 17:48:40 +0800

    Update SAM_prior.py

--- src/SAM_prior.py
+++ src/SAM_prior.py
@@ -13,7 +13,7 @@ output_folder_bo = "../../Datasets/Nuclei_1000/train/boundary_priors/"
 os.makedirs(output_folder_sg, exist_ok=True)
 os.makedirs(output_folder_bo, exist_ok=True)
 
-# 初始化Segment Anything模型和掩码生成器
+# Initialize the Segment Anything model and mask generator
 device = 'cpu'
 sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth")
 sam.to(device=device)
@@ -23,21 +23,21 @@ mask_generator = SamAutomaticMaskGenerator(sam, crop_nms_thresh=0.5, box_nms_thr
 def SAMP(tI, mask_generator):
     masks = mask_generator.generate(tI)
     tI = skimage.img_as_float(tI)
-    SegPrior = np.zeros_like(tI[:, :, 0])  # 使用与输入图像通道相同的数据类型和形状
+    SegPrior = np.zeros_like(tI[:, :, 0])  # 
     BoundaryPrior = np.zeros_like(tI[:, :, 0])
     for maskindex in range(len(masks)):
         thismask = masks[maskindex]['segmentation']
         stability_score = masks[maskindex]['stability_score']
-        thismask_ = thismask.astype(np.uint8)  # 直接转换掩码的数据类型
+        thismask_ = thismask.astype(np.uint8)  # 
         SegPrior[thismask_ == 1] += stability_score
         BoundaryPrior += find_boundaries(thismask_, mode='thick').astype(np.uint8)
         BoundaryPrior[BoundaryPrior > 0] = 1
 
-        # 如果您想将先验信息叠加到原始图像上，请取消注释以下两行
+        # 
     # tI[:, :, 1] = tI[:, :, 1] + SegPrior
     # tI[:, :, 2] = tI[:, :, 2] + BoundaryPrior
 
-    return SegPrior, BoundaryPrior  # 返回先验区域图和轮廓图
+    return SegPrior, BoundaryPrior  # 
 
 
 # 示例使用
@@ -50,18 +50,18 @@ for filename in image_files:
 
     seg_prior, boundary_prior = SAMP(image_array, mask_generator)
 
-    # 归一化先验区域图并应用色彩映射
+    # 
     seg_prior_normalized = (seg_prior - seg_prior.min()) / (seg_prior.max() - seg_prior.min())
     seg_prior_colored = plt.cm.viridis(seg_prior_normalized)[:, :, :3] * 255
     seg_prior_colored = Image.fromarray(seg_prior_colored.astype(np.uint8))
 
-    # 保存先验区域图
+    # Save a priori area map
     seg_prior_output_path = os.path.join(output_folder_sg, f"{os.path.splitext(filename)[0]}_colored_seg_prior.png")
     seg_prior_colored.save(seg_prior_output_path)
 
-    # 保存轮廓图
+    # Save a priori contour map
     boundary_prior_image = Image.fromarray((boundary_prior * 255).astype(np.uint8))
     boundary_prior_output_path = os.path.join(output_folder_bo, f"{os.path.splitext(filename)[0]}_boundary_prior.png")
     boundary_prior_image.save(boundary_prior_output_path)
 
-print("先验图和轮廓图已批量生成并保存到", output_folder_sg, output_folder_bo)
+print("Save to your path", output_folder_sg, output_folder_bo)
